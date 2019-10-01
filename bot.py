87ce8a215c3272b7e9d0d032533f4ca5ff41c334
9999161 <56048872+9999161@users.noreply.github.com>
2019-10-01 23:42:15 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,25 @@
+import pyowm
+import telebot
+
+owm = pyowm.OWM('9571c83a852b5fc9a8e2d5235e36eb2c', language = "ru")
+bot = telebot.TeleBot("803669330:AAHhWsfqru8yUcDthHt_ZtiwctLDU0qwXxI")
+
+@bot.message_handler(content_types=['text'])
+def send_echo(message):
+	observation = owm.weather_at_place( message.text )
+	w = observation.get_weather()
+	temp = w.get_temperature('celsius')["temp"]
+
+	answer = "В городе " + message.text + " сейчас " + w.get_detailed_status() + "\n"
+	answer += "Температура сейчас в районе " + str(temp) + "\n\n"
+
+	if temp < 10:
+		answer += "Сейчас пздц как холодно, одевай подштаники"
+	elif temp < 20:
+		answer += "Сейчас прохладно, но можно гулять"
+	else:
+		answer += "Температура норм"
+
+	bot.send_message(message.chat.id, answer)
+
+bot.polling( none_stop = True )
